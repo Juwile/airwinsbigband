a892fa9328d73cd9118c2f266d694a57e8421448
Juwile <nadine.muelhauser@gmail.com>
2023-02-26 11:38:54 +0100

    Routing set up

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,21 +1,46 @@
 <template>
   <img alt="Logo AIRWIN's Big Band" class="logo" src="./assets/logo_abb.png">
+  <a href="#/">Home</a> |
+  <a href="#/about">About</a> |
+  <a href="#/non-existent-path">Broken Link</a>
   <div class="container">
-    <Home/>
-    <About/>
+    <component :is="currentView" />
   </div>
+  
+  
   
 </template>
 
 <script>
 import Home from './components/Home.vue'
 import About from './components/About.vue'
+import NotFound from './components/NotFound.vue'
+
+const routes = {
+  '/': Home,
+  '/about': About
+}
 
 export default {
   name: 'App',
   components: {
     Home,
     About
+  },
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || '/'] || NotFound
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+		  this.currentPath = window.location.hash
+		})
   }
 }
 </script>
